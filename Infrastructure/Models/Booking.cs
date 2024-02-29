130ebc6344cd6fd75a28894cc56fc5d467bd71b1
ethanharden <ethanjharden3@gmail.com>
2024-02-28 18:05:00 -0800

    Added booking details

--- Infrastructure/Models/Booking.cs
+++ Infrastructure/Models/Booking.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
 
 namespace Infrastructure.Models
 {
     public class Booking
     {
+        [Key]
+        public int Id { get; set; }
+        public int WNumber { get; set; } 
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Note { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public int Duration { get; set; }
+        public string Attatchment {  get; set; }
+
+        [ForeignKey("ProviderId")]
+        public ProviderProfile? ProviderProfile { get; set; }
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
     }
 }
