182290fe90e2f0b5723110f5c7e8125cd1ffa265
prestonjohnson5 <prestonjohnson5@mail.weber.edu>
2024-02-28 19:36:50 -0700

    added date validation to availability

--- Infrastructure/Models/Availability.cs
+++ Infrastructure/Models/Availability.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,59 @@ namespace Infrastructure.Models
 {
     public class Availability
     {
-        int DaysOfWeek {get; set;}   
-        DateTime StartTime { get; set; }
-        DateTime EndTime { get; set; }
-        bool Recurring { get; set; }
-        DateTime RecurringEndDate? { get; set; }
-        bool Unvailable { get; set; } //switched to unavailable so on screen we can ask "Is this Unavailability?" Which makes more sense
+        public int Id { get; set; }
+
+        [Required]
+        public List<DayOfWeek> DaysOfWeek { get; set; } = new List<DayOfWeek>();
+        [Required]
+        [DisplayName("Start Time")]
+        public DateTime StartTime { get; set; }
+        [Required]
+        [DisplayName("End Time")]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public bool Recurring { get; set; }
+        [DisplayName("End Date")]
+        public DateTime? RecurringEndDate { get; set; }
+        [Required]
+        public bool isUnvailable { get; set; } //switched to unavailable so on screen we can ask "Is this Unavailability?" Which makes more sense
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        // Ensuring EndTime is after StartTime
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime must be after StartTime",
+                new[] { nameof(EndTime) });
+        }
+
+        // ensure end date is after current date
+        if (IsRecurring && RecurringEndDate.HasValue && RecurringEndDate.Value.Date <= DateTime.Now.Date)
+        {
+            yield return new ValidationResult(
+                "End Date must be a future date",
+                new[] { nameof(RecurringEndDate) });
+        }
+
+        //ensure end date is after start date
+        if (IsRecurring && RecurringEndDate.HasValue && RecurringEndDate.Value <= StartTime)
+        {
+            yield return new ValidationResult(
+                "End Date must be after Start Date",
+                new[] { nameof(RecurringEndDate) });
+        }
+
+
+        //Use this on the form for Creation, and edit pages
+        //<form method="post">
+        //    < div asp - validation - summary = "ModelOnly" ></ div >
+        //    < button type = "submit" > Submit </ button >
+        //</ form >
+
+        //@section Scripts {
+        //    @{ await Html.RenderPartialAsync("_ValidationScriptsPartial"); }
+        //        }
+        //    }
     }
 }
