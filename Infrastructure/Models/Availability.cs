99f743a71c81ef9424cf577a38ea82c9462442bf
prestonjohnson5 <prestonjohnson5@mail.weber.edu>
2024-02-28 18:52:03 -0700

    Availability Model makde

--- Infrastructure/Models/Availability.cs
+++ Infrastructure/Models/Availability.cs
@@ -8,5 +8,11 @@ namespace Infrastructure.Models
 {
     public class Availability
     {
+        int DaysOfWeek {get; set;}   
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        bool Recurring { get; set; }
+        DateTime RecurringEndDate? { get; set; }
+        bool Unvailable { get; set; } //switched to unavailable so on screen we can ask "Is this Unavailability?" Which makes more sense
     }
 }
