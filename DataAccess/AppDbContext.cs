ec4ee49104ea18dbd158039123a66839b0308043
prestonjohnson5 <prestonjohnson5@mail.weber.edu>
2024-03-01 23:20:51 -0700

    even more repairs

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -9,7 +9,7 @@ using Infrastructure.Models;
 
 namespace DataAccess
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
