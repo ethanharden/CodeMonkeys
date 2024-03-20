beefd3606e1651c8ddf30917558d0b664ad44e00
Torin Lamberson <chaotictorin@yahoo.com>
2024-03-20 16:38:56 -0600

    Index except for dynamic availbilities

--- SchedulingSystemWeb/Pages/Student/Bookings/Upsert.cshtml.cs
+++ SchedulingSystemWeb/Pages/Student/Bookings/Upsert.cshtml.cs
@@ -1,12 +1,23 @@
+using DataAccess;
+using Infrastructure.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Infrastructure.Interfaces;
 
 namespace SchedulingSystemWeb.Pages.Student.Bookings
 {
     public class UpsertModel : PageModel
     {
+       private readonly UnitOfWork _unitOfWork;
+
+        public UpsertModel(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
