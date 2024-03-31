359bc4c4aed9ad8f54f475e1f2eda252352bf1d6
prestonjohnson5 <prestonjohnson5@mail.weber.edu>
2024-03-30 21:22:31 -0600

    dropdown work

--- SchedulingSystemWeb/Pages/Student/ListOfProviders.cshtml.cs
+++ SchedulingSystemWeb/Pages/Student/ListOfProviders.cshtml.cs
@@ -4,57 +4,46 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace SchedulingSystemWeb.Pages.Student
 {
     public class ListOfProvidersModel : PageModel
     {
         private readonly UnitOfWork _unitOfWork;
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public List<ApplicationUser> objApplicationUserList;
         public Dictionary<string, IList<string>> UserRoles;
         public IEnumerable<Department> departmentList;
-        public ListOfProvidersModel(UnitOfWork unitofWork, UserManager<ApplicationUser> userManager)
+        public IList<IdentityRole> Roles;
+
+        public ListOfProvidersModel(UnitOfWork unitofWork, RoleManager<IdentityRole> roleManager)
         {
-            _unitOfWork = unitofWork;
-            objApplicationUserList = new List<ApplicationUser>();
-            _userManager = userManager;
-            UserRoles = new Dictionary<string, IList<string>>(); // Initialize user roles dictionary
-            
+            _unitOfWork = unitofWork;      
+            _roleManager = roleManager;
         }
+
         public async Task<IActionResult> OnGetAsync(string search, string role)
         {
             departmentList = _unitOfWork.Department.GetAll();
+            Roles = await _roleManager.Roles.ToListAsync();
 
-            //    if (!string.IsNullOrEmpty(role))
-            //    {
-            //        objApplicationUserList.AddRange(await _userManager.GetUsersInRoleAsync(role));
 
-            //    }
-            //    else
-            //    {
-            //        objApplicationUserList.AddRange(await _userManager.GetUsersInRoleAsync("TUTOR"));
-            //        objApplicationUserList.AddRange(await _userManager.GetUsersInRoleAsync("TEACHER"));
-            //        objApplicationUserList.AddRange(await _userManager.GetUsersInRoleAsync("ADVISOR"));
-            //    }
-            //    if (!string.IsNullOrEmpty(search))
-            //    {
-            //        objApplicationUserList = objApplicationUserList.Where(u => u.FullName.ToLower().Contains(search.ToLower())).ToList();
+            return Page();
+        }
 
-            //    }
-
-            //    foreach (var user in objApplicationUserList)
-            //    {
-            //        UserRoles[user.Id] = await _userManager.GetRolesAsync(user);
-            //    }
-
-                return Page();
-            }
-
-            public async Task<IActionResult> OnPostAsync(string role, int department)
+        public async Task<IActionResult> OnGetListProvidersAsync(string role, int department)
         {
+            var providerProfiles = _unitOfWork.ProviderProfile
+                .GetAll(pp => pp.DeparmentId == department /*&&  Condition to match the role */)
+                .Select(pp => new { pp.Id, pp.userFullName })
+                .ToList();
 
+            return new JsonResult(providerProfiles);
+        }
+
+        public async Task<IActionResult> OnPostAsync(string role, int department)
+        {
             return RedirectToPage("/Student/Bookings/Index", new { role = role, department = department});
         }
         
