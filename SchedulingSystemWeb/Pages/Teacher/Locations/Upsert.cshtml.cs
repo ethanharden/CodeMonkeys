935b707fb0b00a60432cb0d0f797b56266f8c74a
prestonjohnson5 <prestonjohnson5@mail.weber.edu>
2024-04-17 15:56:31 -0600

    Location upsert created

--- SchedulingSystemWeb/Pages/Teacher/Locations/Upsert.cshtml.cs
+++ SchedulingSystemWeb/Pages/Teacher/Locations/Upsert.cshtml.cs
@@ -12,22 +12,57 @@ namespace SchedulingSystemWeb.Pages.Teacher.Locations
     {
         private readonly UnitOfWork _unitOfWork;
         private readonly UserManager<ApplicationUser> _userManager;
+        [BindProperty]
+        public Location objLocation { get; set; }
+        public IEnumerable<LocationType> typeList { get; set; }
 
         public UpsertModel(UnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
         {
             _unitOfWork = unitOfWork;
             _userManager = userManager;
-            
+            typeList = new List<LocationType>();
         }
 
-        public void OnGet(int? locationId)
+        public IActionResult OnGet(int? locationId)
         {
+            typeList = _unitOfWork.LocationType.GetAll();
 
+            if (locationId.HasValue)
+            {
+                objLocation = _unitOfWork.Location.GetById(locationId.Value);
+                if (objLocation == null)
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                objLocation = new Location();
+            }
+            return Page();
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
 
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            objLocation.ProfileId = _unitOfWork.ProviderProfile.Get(p => p.User == _userManager.GetUserId(User)).Id;
+            if(objLocation.LocationId == 0)// Adding a new
+            {
+                _unitOfWork.Location.Add(objLocation);
+            }
+            else// editing existing
+            {
+                _unitOfWork.Location.Update(objLocation);
+            }
+
+            await _unitOfWork.CommitAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
