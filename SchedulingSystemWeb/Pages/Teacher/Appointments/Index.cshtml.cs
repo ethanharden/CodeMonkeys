5e0caee7bac1df8f82c626bd5f82ff8d9719c4b1
Trevor <sheridad33@gmail.com>
2024-04-19 20:03:43 -0600

    stuff

--- SchedulingSystemWeb/Pages/Teacher/Appointments/Index.cshtml.cs
+++ SchedulingSystemWeb/Pages/Teacher/Appointments/Index.cshtml.cs
@@ -1,10 +1,20 @@
 using DataAccess;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using Infrastructure.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace SchedulingSystemWeb.Pages.Teacher.Appointments
 {
@@ -27,6 +37,8 @@ namespace SchedulingSystemWeb.Pages.Teacher.Appointments
         public ApplicationUser teacher { get; set; }
         public List<string> filepath { get; set; }
         public Location location { get; set; }
+        public Availability aval { get; set; }
+        public CustomerProfile studprofile { get; set; }
         public async Task OnGet(int? id )
         {
 
@@ -35,8 +47,9 @@ namespace SchedulingSystemWeb.Pages.Teacher.Appointments
             booking = _unitOfWork.Booking.Get(b => b.Id == id & b.ProviderProfileID ==provId);
             teacher = _unitOfWork.ApplicationUser.Get(u => u.Id == _userManager.GetUserId(User));
             student = _unitOfWork.ApplicationUser.Get(s => s.Id == booking.User);
-            var aval = _unitOfWork.Availability.Get(i => i.Id == booking.objAvailability);
+            aval = _unitOfWork.Availability.Get(i => i.Id == booking.objAvailability);
             location = _unitOfWork.Location.Get(l=> l.LocationId == aval.LocationId);
+            studprofile = _unitOfWork.CustomerProfile.Get(d => d.User == student.Id);
             filepath = new List<string>();
             if (booking.Attachment != null && booking.Attachment.Count() > 0)
             {
@@ -60,9 +73,57 @@ namespace SchedulingSystemWeb.Pages.Teacher.Appointments
             }
 
         }
-        public async Task OnPost()
+        public async Task OnPost(int? id)
         {
-            //Add calendar thinggy
+            int provId = _unitOfWork.ProviderProfile.Get(p => p.User == _userManager.GetUserId(User)).Id;
+            booking = _unitOfWork.Booking.Get(b => b.Id == id & b.ProviderProfileID == provId);
+            teacher = _unitOfWork.ApplicationUser.Get(u => u.Id == _userManager.GetUserId(User));
+            student = _unitOfWork.ApplicationUser.Get(s => s.Id == booking.User);
+            aval = _unitOfWork.Availability.Get(i => i.Id == booking.objAvailability);
+            location = _unitOfWork.Location.Get(l => l.LocationId == aval.LocationId);
+            TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
+            UserCredential credential;
+            string[] Scopes = { Google.Apis.Calendar.v3.CalendarService.Scope.CalendarEvents };
+            string ApplicationName = "Code Monkeys";
+            string CalendarId = "primary";
+            Event newEvent = new()
+            {
+                Summary = booking.Subject,
+                Location = _unitOfWork.Location.Get(l => l.LocationId == aval.LocationId).LocationName,
+                Description = booking.Note,
+                Start = new EventDateTime()
+                {
+                    DateTime = booking.StartTime,
+                    TimeZone = localTimeZone.DisplayName,
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = booking.StartTime.AddMinutes(booking.Duration),
+                    TimeZone = localTimeZone.DisplayName,
+                },
+            };
+            using (var stream = new FileStream(@"wwwroot/secret/client_secret_328091863377-8nbrlurrfk4f51on47vvosc341ajlgk1.apps.googleusercontent.com (1).json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            // Create Google Calendar API service.
+            var service = new Google.Apis.Calendar.v3.CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "CodeMonkeys"
+            });
+            EventsResource.InsertRequest request = service.Events.Insert(newEvent, CalendarId);
+            Google.Apis.Calendar.v3.Data.Event createdEvent = request.Execute();
+            Console.WriteLine("Event created: {0}", createdEvent.HtmlLink);
         }
     }
 }
